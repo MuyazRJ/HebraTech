c0d01bc1af4dee9925c30719abd4a803470bf180
Muyaz Rahman <Muyaz154@gmail.com>
2023-08-22 18:46:57 +0100

    screens

--- components/FoundersLanding.tsx
+++ components/FoundersLanding.tsx
@@ -7,10 +7,10 @@ const FoundersLanding = () => {
     return ( 
         <div className="bg-main-black pt-12 lg:pt-20 h-full relative lg:pb-10">
             <div className="relative max-w-[1920px] mx-auto">
-            <div className="absolute top-0 left-0 translate-x-[-260px] translate-y-[-217px] lg:block hidden xl:translate-x-[-240px] xl:translate-y-[-245px] 2xl:translate-x-[-130px]">
-                    <Lottie autoPlay={true} loop={true} animationData={animationData} className="w-[890px] xl:w-[1100px] 2xl:w-[1200px] h-auto"/>
+            <div className="absolute top-0 left-0 translate-x-[-260px] translate-y-[-217px] lg:block hidden xl:translate-x-[-240px] xl:translate-y-[-245px] 3xl:translate-x-[-130px] 2xl:translate-x-[-120px]">
+                    <Lottie autoPlay={true} loop={true} animationData={animationData} className="w-[890px] xl:w-[1100px] 3xl:w-[1200px] h-auto"/>
             </div>
-            <div className="absolute translate-x-[-45%] mt-4 w-[430px] h-[310px] md:h-[365px] lg:translate-x-[4.65rem] lg:w-[220px] 2xl:w-[290px] lg:translate-y-[1.85rem] xl:translate-x-[11.2rem] xl:translate-y-[6.8rem] xl:w-[260px] 2xl:translate-x-[20.3rem] 2xl:translate-y-[9.8rem]">
+            <div className="absolute translate-x-[-45%] mt-4 w-[430px] h-[310px] md:h-[365px] lg:translate-x-[4.65rem] lg:w-[220px] 3xl:w-[290px] lg:translate-y-[1.85rem] xl:translate-x-[11.2rem] xl:translate-y-[6.8rem] xl:w-[260px] 2xl:translate-x-[18.8rem] 3xl:translate-x-[20.3rem] 3xl:translate-y-[9.8rem]">
             <Image
                 src="/hebratech_logo.png"
                 alt='hebratech logo'
@@ -22,9 +22,9 @@ const FoundersLanding = () => {
             <div className="flex mx-auto items-center justify-center flex-col max-w-[1600px] w-[85%]">
                 <TextReveal><h2 className="text-white xl:pb-6 lg:pb-2 text-2xl 2xl:text-8xl lg:text-7xl lg:text-[4.3rem] md:text-3xl font-semibold lg:before:hidden text-center mb-8 before:absolute relative before:border-b-[1px] before:border-[#A3A3A3] before:w-[110%] before:right-[-5%] before:top-10 before:bottom-0 before:opacity-50">MEET OUR FOUNDERS</h2></TextReveal>
                 
-                <p className="text-[#C3C3C3] md:text-lg z-[1] lg:absolute lg:text-base lg:w-[33%] lg:translate-y-[128px] lg:translate-x-[4rem] lg:left-0 xl:translate-y-[188px] xl:translate-x-[7rem] 2xl:text-lg 2xl:translate-x-[10.5rem] 2xl:w-[40%]">HeBra Technologies is led by a visionary team of founders, each bringing diverse expertise to the table. Their collective knowledge in optics, mathematics, physics, and manufacturing drives groundbreaking innovations in optical computing and related technologies.</p>
+                <p className="text-[#C3C3C3] md:text-lg z-[1] lg:absolute lg:text-base lg:w-[33%] lg:translate-y-[128px] lg:translate-x-[4rem] lg:left-0 xl:translate-y-[188px] xl:translate-x-[7rem] 2xl:text-lg 2xl:translate-x-[9rem] 2xl:w-[37%]">HeBra Technologies is led by a visionary team of founders, each bringing diverse expertise to the table. Their collective knowledge in optics, mathematics, physics, and manufacturing drives groundbreaking innovations in optical computing and related technologies.</p>
 
-                <button type="button" className="radial mt-8 self-start lg:absolute lg:left-0 lg:lg:translate-y-[248px] lg:translate-x-[4rem] xl:translate-y-[298px] xl:translate-x-[7rem] 2xl:translate-x-[10.5rem]">
+                <button type="button" className="radial mt-8 self-start lg:absolute lg:left-0 lg:lg:translate-y-[248px] lg:translate-x-[4rem] xl:translate-y-[298px] xl:translate-x-[7rem] 2xl:translate-x-[9rem]">
                     <span className="font-semibold text-sm tracking-tight text-white md:text-base">SEE MORE OF THE TEAM</span>
                 </button>
 
