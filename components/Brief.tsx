5c25d98b962c01e4db870b0b81cf368cd37518bd
Muyaz Rahman <Muyaz154@gmail.com>
2023-08-23 15:41:43 +0100

    screens

--- components/Brief.tsx
+++ components/Brief.tsx
@@ -9,9 +9,9 @@ const Brief = () => {
                 <TextReveal>
                 <h2 className="text-white md:text-3xl z-[1] text-2xl 2xl:text-8xl lg:text-7xl lg:text-[4.3rem] font-semibold text-center mb-8 before:absolute relative before:border-b-[1px] before:border-[#A3A3A3] before:w-[110%] before:right-[-5%] before:top-10 before:bottom-0 before:opacity-50 lg:before:hidden">HEBRA TECHNOLOGIES</h2></TextReveal>
 
-                <p className="text-[#C3C3C3] md:text-lg 2xl:pt-10 xl:pt-4 2xl:text-xl z-[1]">HeBra Technologies specializes in photon-based  technology, focusing on quantum-powered computing devices and superpowered communication networks through long-range laser communications.<br/><br/>At HeBra Technologies, we have invented and patented two versions of our tech devices. One will quantum power all computing devices, with the equivalent of 2 million qubits, and the other will superpower all communications networks, using long-range laser communications through the atmosphere. Both use virtually no energy, making them extremely climate friendly.</p>
+                <p className="text-[#C3C3C3] md:text-lg lg:pt-10 2xl:text-xl z-[1]">HeBra Technologies specializes in photon-based  technology, focusing on quantum-powered computing devices and superpowered communication networks through long-range laser communications.<br/><br/>At HeBra Technologies, we have invented and patented two versions of our tech devices. One will quantum power all computing devices, with the equivalent of 2 million qubits, and the other will superpower all communications networks, using long-range laser communications through the atmosphere. Both use virtually no energy, making them extremely climate friendly.</p>
 
-                <div className="self-start xl:pt-10"><CustomButton text="Meet Our Team" styles="mt-10" boxShadow="0 0 12.5em 0 #FFB800"/></div>
+                <div className="self-start lg:pt-10 lg:pb-6"><CustomButton text="Meet Our Team" styles="mt-10" boxShadow="0 0 12.5em 0 #FFB800"/></div>
             </div>
         </div>
      );
--- components/InfoLanding.tsx
+++ components/InfoLanding.tsx
@@ -1,42 +1,37 @@
 
 import Image from "next/image";
 
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import infoAData  from '../animations/new_age_of_computing.json'
+import infoSData  from '../animations/super_communications.json'
+
 const InfoLanding = () => {
     return ( 
-        <div className="bg-main-black pt-12 h-full flex flex-col items-center pb-10">
-            <div className="mt-4 w-[75%] z-[1] mb-10" style={{borderBottom: '2px solid rgba(255, 255, 255, .75)'}}/>
-            <div className="flex flex-col items-center max-w-[85%]">
-                <Image
-                    src="/filler.png"
-                    alt='hebratech logo'
-                    className='object-contain'
-                    width={50}
-                    height={50}
-                />
+        <div className="bg-main-black xl:pt-20 pt-8 h-full items-center pb-20">
+            <div className="mt-4 w-[75%] z-[1] mb-10 mx-auto lg:hidden" style={{borderBottom: '2px solid rgba(255, 255, 255, .75)'}}/>
 
-                <h2 className="text-white font-semibold text-xl mt-6">The New-Age Of Computing</h2>
-                <p className="text-[#C3C3C3] mt-4  w-[100%]">The quantum-powered computing devices offer exceptional capabilities, including the equivalent of 2 million qubits and operating speeds hundreds of millions of times faster than traditional computing. These devices generate no heat and address the limitations of digital computing, providing a solution to the growing computational power crisis.</p>
-                <button type="button" className="radial mt-8 self-start" >
-                    <span className="font-semibold text-md tracking-tight text-white">Learn More</span>
-                </button>
-            </div>
+            <div className="mx-auto flex-col items-center justify-center flex lg:flex-row lg:w-[85%] lg:gap-[6rem] max-w-[1600px]">
+                <div className="info_container">
+                <div className="relative"><Lottie autoPlay={true} loop={true} animationData={infoAData} className="w-[130px] lg:w-[150px] xl:w-[200px] 3xl:w-[230px] h-auto"/></div>
 
-            <div className="w-[45%] mt-10 mb-10" style={{borderBottom:'1px solid rgba(255, 255, 255, .75)'}}/>
+                    <h2 className="info_t">The New-Age Of Computing</h2>
+                    <p className="info_p">The quantum-powered computing devices offer exceptional capabilities, including the equivalent of 2 million qubits and operating speeds hundreds of millions of times faster than traditional computing. These devices generate no heat and address the limitations of digital computing, providing a solution to the growing computational power crisis.</p>
+                    <button type="button" className="radial mt-8 self-start" >
+                        <span className="info_s">Learn More</span>
+                    </button>
+                </div>
 
-            <div className="flex flex-col items-center max-w-[85%]">
-                <Image
-                    src="/filler2.png"
-                    alt='hebratech logo'
-                    className='object-contain'
-                    width={50}
-                    height={50}
-                />
+                <div className="w-[45%] mt-10 mb-10 lg:hidden" style={{borderBottom:'1px solid rgba(255, 255, 255, .75)'}}/>
 
-                <h2 className="text-white font-semibold text-xl mt-6">Super Communications</h2>
-                <p className="text-[#C3C3C3] mt-4 w-[100%]">On the other hand, the superpowered communication networks enable long-range laser communications that can transmit data through the atmosphere and weather. This advancement has the potential to greatly enhance and revolutionize global communication systems by significantly improving efficiency and reliability.</p>
-                <button type="button" className="radial mt-8 self-start" >
-                    <span className="font-semibold text-md tracking-tight text-white">Learn More</span>
-                </button>
+                <div className="info_container">
+                    <div className="relative"><Lottie autoPlay={true} loop={true} animationData={infoSData} className="w-[130px] lg:w-[150px] xl:w-[200px] 3xl:w-[230px] h-auto"/></div>
+
+                    <h2 className="info_t">Super Communications</h2>
+                    <p className="info_p">On the other hand, the superpowered communication networks enable long-range laser communications that can transmit data through the atmosphere and weather. This advancement has the potential to greatly enhance and revolutionize global communication systems by significantly improving efficiency and reliability.</p>
+                    <button type="button" className="radial mt-8 self-start" >
+                        <span className="info_s">Learn More</span>
+                    </button>
+                </div>
             </div>
         </div>
      );
