75fe12133893a4521b7f32101f0b3dbd81979e50
Muyaz Rahman <Muyaz154@gmail.com>
2023-08-23 18:39:19 +0100

    contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,23 +8,25 @@ const Contact = () => {
     const [email, setEmail] = useState('')
 
     return ( 
-        <div className="w-full bg-[#F2F2F2] pt-8 flex flex-col items-center z-0 relative">
-            <h2 className="text-[2rem] font-bold leading-[2.3rem] z-[1]">SPEAK WITH US</h2>
-            <span className="font-bold text-md z-[1]">we'll get back to you</span>
+        <div className="w-full bg-[#F2F2F2] pt-8 flex flex-col items-center z-0 relative lg:flex-row-reverse justify-center lg:gap-16 2xl:gap-24">
+            <div className="relative flex flex-col items-center lg:mb-16 lg:items-start">
+                <h2 className="text-[2rem] font-bold leading-[2.3rem] z-[1] sm:text-[2.9rem] md:text-[3.3rem] lg:text-[3.7rem] xl:text-[4rem] 2xl:text-[4.4rem]">SPEAK WITH US</h2>
+                <span className="font-bold text-md z-[1] sm:text-lg md:text-xl md:mt-2 lg:tracking-wide lg:text-2xl lg:mt-4 xl:text-3xl 2xl:text-[2.1rem] 2xl:mt-6">we'll get back to you</span>
 
-            <div className="flex gap-[1rem] w-screen absolute translate-y-[-17px]" aria-hidden="true">
-                <span className="marquee">GET IN TOUCH</span>
-                <span className="marquee">GET IN TOUCH</span>
+                <div className="flex gap-[1rem] w-screen absolute translate-y-[-18px] lg:translate-y-[-24px] xl:translate-y-[-33px] -z-[1] 3xl:translate-y-[-44px]" aria-hidden="true">
+                    <span className="marquee">GET IN TOUCH</span>
+                    <span className="marquee">GET IN TOUCH</span>
+                </div>
             </div>
 
             <form className="mt-6 flex flex-col">
                 <div className="flex justify-between gap-4">
-                    <input type="text" name="name" placeholder="name" className="input-box w-[9.5rem]" maxLength={50} value={name} onChange={e => setName(e.target.value)}/>
-                    <input type="text" name="phone" placeholder="phone" className="input-box w-[9.5rem]" maxLength={13} value={phone} onChange={e => setPhone(e.target.value)}/>
+                    <input type="text" name="name" placeholder="name" className="input-box w-[9.5rem] sm:w-[14rem] md:w-[16rem] lg:w-[11rem] xl:w-[13rem] lg:mb-2 2xl:w-[15rem] 2xl:mb-4" maxLength={50} value={name} onChange={e => setName(e.target.value)}/>
+                    <input type="text" name="phone" placeholder="phone" className="input-box w-[9.5rem] sm:w-[14rem] md:w-[16rem] lg:w-[11rem] xl:w-[13rem] lg:mb-2 2xl:w-[15rem] 2xl:mb-4" maxLength={13} value={phone} onChange={e => setPhone(e.target.value)}/>
                 </div>
 
                 <input type="text" name="email" placeholder="email" className="input-box w-full mt-4" maxLength={60} value={email} onChange={e => setEmail(e.target.value)}/>
-                <input type="submit" className="bg-main-gold text-black px-[3.25rem] py-2 rounded-full font-bold shadow-lg hover:scale-110 duration-200 cursor-pointer self-center justify-self-end mt-4 mb-10"/>
+                <input type="submit" className="bg-main-gold text-black px-[3.25rem] py-2 rounded-full font-bold shadow-lg hover:scale-110 duration-200 cursor-pointer self-center justify-self-end mt-4 mb-10 lg:mt-6 2xl:text-lg 2xl:mt-8"/>
             </form>
         </div>
      );
--- components/InfoLanding.tsx
+++ components/InfoLanding.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 
 import Lottie, { LottieRefCurrentProps } from "lottie-react";
-import infoAData  from '../animations/new_age_of_computing.json'
+import infoAData  from '../animations/new_age_of_computing_4.json'
 import infoSData  from '../animations/super_communications.json'
 
 const InfoLanding = () => {
@@ -10,27 +10,33 @@ const InfoLanding = () => {
         <div className="bg-main-black xl:pt-20 pt-8 h-full items-center pb-20">
             <div className="mt-4 w-[75%] z-[1] mb-10 mx-auto lg:hidden" style={{borderBottom: '2px solid rgba(255, 255, 255, .75)'}}/>
 
-            <div className="mx-auto flex-col items-center justify-center flex lg:flex-row lg:w-[77%] lg:gap-[6rem] max-w-[1320px] 3xl:w-[66%]">
+            <div className="mx-auto flex-col items-center justify-center flex lg:flex-row lg:w-[80%] xl:w-[77%] lg:gap-[6rem] max-w-[1320px] 3xl:w-[66%]">
                 <div className="info_container">
-                <div className="relative"><Lottie autoPlay={true} loop={true} animationData={infoAData} className="w-[130px] lg:w-[150px] xl:w-[200px] 3xl:w-[230px] h-auto"/></div>
+                <div className="relative"><Lottie autoPlay={true} loop={true} animationData={infoAData} className="w-[195px] lg:w-[238px] xl:w-[320px] 3xl:w-[340px] h-auto"/></div>
 
+                <div className="info_line">
                     <h2 className="info_t">The New-Age Of Computing</h2>
                     <p className="info_p">The quantum-powered computing devices offer exceptional capabilities, including the equivalent of 2 million qubits and operating speeds hundreds of millions of times faster than traditional computing. These devices generate no heat and address the limitations of digital computing, providing a solution to the growing computational power crisis.</p>
+                    
                     <button type="button" className="radial mt-8 self-start" >
                         <span className="info_s">Learn More</span>
                     </button>
+                </div>
                 </div>
 
                 <div className="w-[45%] mt-10 mb-10 lg:hidden" style={{borderBottom:'1px solid rgba(255, 255, 255, .75)'}}/>
 
                 <div className="info_container">
-                    <div className="relative"><Lottie autoPlay={true} loop={true} animationData={infoSData} className="w-[130px] lg:w-[150px] xl:w-[200px] 3xl:w-[230px] h-auto"/></div>
+                    <div className="relative"><Lottie autoPlay={true} loop={true} animationData={infoSData} className="w-[130px] lg:w-[150px] xl:w-[190px] 3xl:w-[220px] h-auto"/></div>
 
-                    <h2 className="info_t">Super Communications</h2>
-                    <p className="info_p">On the other hand, the superpowered communication networks enable long-range laser communications that can transmit data through the atmosphere and weather. This advancement has the potential to greatly enhance and revolutionize global communication systems by significantly improving efficiency and reliability.</p>
-                    <button type="button" className="radial mt-8 self-start" >
-                        <span className="info_s">Learn More</span>
-                    </button>
+                    <div className="info_line">
+                        <h2 className="info_t">Super Communications</h2>
+                        <p className="info_p">On the other hand, the superpowered communication networks enable long-range laser communications that can transmit data through the atmosphere and weather. This advancement has the potential to greatly enhance and revolutionize global communication systems by significantly improving efficiency and reliability.</p>
+
+                        <button type="button" className="radial mt-8 self-start" >
+                            <span className="info_s">Learn More</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
