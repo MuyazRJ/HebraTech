abd2d624edcc860ea9169b212468e52169528b28
Muyaz Rahman <Muyaz154@gmail.com>
2023-08-25 18:27:36 +0100

    founders

--- components/Team.tsx
+++ components/Team.tsx
@@ -41,15 +41,16 @@ const Team = () => {
     }, [member])
 
     return ( 
-        <div className="bg-main-black pb-10">
+        <div className="bg-main-black pb-10 relative">
             <TextReveal>
                 <div className="flex justify-center"><h2 className="founders_text z-[32]">KEY TEAM MEMBERS</h2></div>
             </TextReveal>
 
             <div className="flex flex-col justify-center w-[90%] mx-auto items-center mt-8">
- 
+                <div className="absolute w-screen h-full">
 
-                <div className="w-[120vw] relative">
+                </div>
+                <div className="w-[120vw] relative hidden">
                     <div className="w-full h-[0.1px] bg-main-gold absolute translate-y-[13.5rem] rotate-[29deg] bg-gradient-to-r from-[#FFA800] to-[#FFB800] opacity-80"/>
                     <div className="w-full h-[0.1px] bg-main-gold absolute translate-y-[7.5rem] rotate-[29deg] opacity-30"/>
                     <div className="w-full h-[0.1px] bg-main-gold absolute translate-y-[5rem] rotate-[29deg] opacity-30"/>
@@ -58,21 +59,34 @@ const Team = () => {
                     <div className="w-[70%] h-[0.1px] bg-main-gold absolute translate-y-[17.5rem] rotate-[29deg] opacity-60"/>
                 </div>
                 
-                <div className="flex gap-6">
-                    <Hexagon imgPath="GARY_K._FEDDER_PhD.png" size="100" alt="GARY K. FEDDER" click={() => setMember(4)} currentMember={member} memberNumber={4}/>
-                    <Hexagon imgPath="PATRICK_J._REARDON_PhD.png" size="100" alt="PATRICK J. REARDON" click={() => setMember(2)} currentMember={member} memberNumber={2}/>
-                </div>
+                <div className="flex flex-col items-center relative">
+                    <div className="absolute">
+                    <div className="relative w-[450px] h-[450px]">
+                    <Image
+                        src="/lines_shooting.png"
+                        alt='Founder Anthony Hester'
+                        fill={true}
+                        className='absolute scale-150 object-contain -translate-y-4'
+                    />
+                    </div>
+                    </div>
 
-                <div className="flex gap-6">
-                    <Hexagon imgPath="DAVID_M._BLOOM_PhD_Stanford_Team_Professor.png" size="100" alt="DAVID M. BLOOM" click={() => setMember(3)} currentMember={member} memberNumber={3}/>
-                    <Hexagon imgPath="JUAN_ALONSO_Director_of_Simulations.png" size="100" alt="JUAN ALONSO" click={() => setMember(5)} currentMember={member} memberNumber={5}/>
-                    <Hexagon imgPath="CHRISTINA_BUTLER_Director_of_Government_Affairs_-HeBraTech_1.png" size="100" alt="CHRISTINA BUTLER" click={() => setMember(1)} currentMember={member} memberNumber={1}/>
-                </div>
+                    <div className="flex gap-6">
+                        <Hexagon imgPath="GARY_K._FEDDER_PhD.png" size="100" alt="GARY K. FEDDER" click={() => setMember(4)} currentMember={member} memberNumber={4}/>
+                        <Hexagon imgPath="PATRICK_J._REARDON_PhD.png" size="100" alt="PATRICK J. REARDON" click={() => setMember(2)} currentMember={member} memberNumber={2}/>
+                    </div>
+
+                    <div className="flex gap-6">
+                        <Hexagon imgPath="DAVID_M._BLOOM_PhD_Stanford_Team_Professor.png" size="100" alt="DAVID M. BLOOM" click={() => setMember(3)} currentMember={member} memberNumber={3}/>
+                        <Hexagon imgPath="JUAN_ALONSO_Director_of_Simulations.png" size="100" alt="JUAN ALONSO" click={() => setMember(5)} currentMember={member} memberNumber={5}/>
+                        <Hexagon imgPath="CHRISTINA_BUTLER_Director_of_Government_Affairs_-HeBraTech_1.png" size="100" alt="CHRISTINA BUTLER" click={() => setMember(1)} currentMember={member} memberNumber={1}/>
+                    </div>
 
 
-                <div className="flex gap-6">
-                    <Hexagon imgPath="CLAUDIO_MORALES_PhD_Mathematician__Algorithms_Expert.png" size="100" alt="CLAUDIO MORALES" click={() => setMember(6)} currentMember={member} memberNumber={6}/>
-                    <Hexagon imgPath="BOBBY_DENEEFE_Financial_Advisor_and_Board_Member.png" size="100" alt="BOBBY DENEEFE" click={() => setMember(7)} currentMember={member} memberNumber={7}/>
+                    <div className="flex gap-6">
+                        <Hexagon imgPath="CLAUDIO_MORALES_PhD_Mathematician__Algorithms_Expert.png" size="100" alt="CLAUDIO MORALES" click={() => setMember(6)} currentMember={member} memberNumber={6}/>
+                        <Hexagon imgPath="BOBBY_DENEEFE_Financial_Advisor_and_Board_Member.png" size="100" alt="BOBBY DENEEFE" click={() => setMember(7)} currentMember={member} memberNumber={7}/>
+                    </div>
                 </div>
 
                 <div className="border-l-[3.5px] border-main-gold ml-[1.2rem] mt-10 overflow-x-hidden relative mb-2">
